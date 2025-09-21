39237cdd4e78335f6488a72fdbc170356d74d10f
Naira Almelegy <musicforever384@gmail.com>
2025-09-21 20:52:27 +0300

    fix: Update background color in PatientLayout for consistent styling

--- src/pages/PatientLayout.tsx
+++ src/pages/PatientLayout.tsx
@@ -23,9 +23,9 @@ export default function PatientLayout() {
   };
 
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen bg-gray-50">
       <PatientSidebar>
-      <main className="flex-1  overflow-y-auto bg-gray-50 min-h-screen">
+      <main className="flex-1 overflow-y-auto min-h-screen">
         <div className="max-w-4xl mx-auto py-8 px-4">
           <Outlet />
         </div>
